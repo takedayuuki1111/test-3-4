94212df0fc50f2f0d3dea8c3c97d7c9ce114626a
Takeda Yuuki <takedayuuki0102@gmail.com>
2025-09-04 15:57:54 +0900

    ただのフォルダーとしての運用

---
+++ src/php01/index04.php
@@ -0,0 +1,13 @@
+<?php
+$a = 15;
+$b = 3;
+$c = 10;
+$d = 5;
+
+$a = $b;
+$c += $d;
+
+echo $a;
+echo "<br />";
+echo $c;
+echo "<br />";
---
+++ src/php01/index07.php
@@ -0,0 +1,9 @@
+<?php
+function tasizan($number1, $number2)
+{
+  $value = $number1 + $number2;
+  return $value;
+}
+
+$total = tasizan(2, 4);
+echo $total;
